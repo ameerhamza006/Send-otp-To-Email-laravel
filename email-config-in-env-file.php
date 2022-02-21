fb92c3ad40a9d5a02d006914c69c70bc502567d4
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2022-02-21 21:03:24 +0500

    Create email-config-in-env-file.php

---
+++ email-config-in-env-file.php
@@ -0,0 +1,10 @@
+
+
+MAIL_MAILER=sendmail
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USERNAME=noreply@gmail.com //your email
+MAIL_PASSWORD=secret // your email password 
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=noreply@crm.ioncapitalsolutions.com  //your email
+MAIL_FROM_NAME=CRM  //your Company Name
