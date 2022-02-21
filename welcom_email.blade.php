cc64d6ee78090ff6aff916a7fee717c77cb26501
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2022-02-21 20:50:48 +0500

    email inbox layout

---
+++ welcom_email.blade.php
@@ -0,0 +1,15 @@
+
+Dear {{ $name }},<br><br>
+
+
+
+We received a request to access your EXAMPLE Account {{$email}} through your email address. Your EXAMPLE verification code is: <br> <br>
+
+<h3>{{$otp}}</h3>
+
+<br><br>
+
+
+
+Thank You,<br>
+<h4>EXAMPLE</h4>
